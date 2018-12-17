1c047cf46ebc6aa35ab21aa7d392e091c81ed807
Sergii Nezdolii <sergii@frost.se>
2018-12-17 14:16:02 +0100

    [ADD] adding camera permission check

--- ardemo/app/src/main/java/se/frost/ardemo/MainActivity.kt
+++ ardemo/app/src/main/java/se/frost/ardemo/MainActivity.kt
@@ -2,11 +2,31 @@ package se.frost.ardemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this)
+            return
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, R.string.camera_permission, Toast.LENGTH_LONG).show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
+        }
     }
 }
